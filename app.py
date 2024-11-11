c15c4aa6d03913b3a3df3368330ee219d99e227d
Narender Surabhi <iamnarenderrao@gmail.com>
2024-11-10 21:48:35 -0500

    , host="0.0.0.0",

--- app.py
+++ app.py
@@ -24,4 +24,4 @@ def get_response():
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 3050))
-    app.run(debug=True, port=port)
+    app.run(debug=True, host="0.0.0.0", port=port)
